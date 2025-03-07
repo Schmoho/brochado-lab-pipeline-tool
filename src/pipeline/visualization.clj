a8657f20dc1b35f1048955aebb72dca749cb9031
Dario <d.eltzner@gmx.de>
2025-03-07 15:37:59 +0100

    cool viz

--- src/pipeline/visualization.clj
+++ src/pipeline/visualization.clj
@@ -1,54 +1,116 @@
 (ns pipeline.visualization
   (:require
-   [oz.core :as oz]))
+   [data-cleaning :as clean]
+   [oz.core :as oz]
+   [clojure.data.csv :as csv]
+   [clojure.java.io :as io]
+   [clojure.string :as str]))
+
+(defn csv-lines->maps
+  [csv-lines & {:keys [keywordize?] :or {keywordize? true}}]
+  (map zipmap
+       (->> (first csv-lines)
+            (map
+             (comp
+              (if keywordize?
+                keyword
+                identity)
+              #(str/replace % "." "_")))
+            repeat)
+       (rest csv-lines)))
+
+(defn field-domain
+  [field csv-data]
+  (set (map field csv-data)))
+
+(def data
+  (->> (io/reader "resources/tpp-raw-cefotaxime-ecoli.csv")
+       csv/read-csv
+       csv-lines->maps
+       (transduce
+        (comp
+         (clean/filtering-insanity
+          {:protein_id #(str/starts-with? % "#")})
+         clean/numerify)
+        conj
+        [])))
+
+(def processed
+  (->> (io/reader "resources/tpp-processed-cefotaxime-ecoli.csv")
+       csv/read-csv
+       csv-lines->maps
+       (transduce
+        (comp
+         #_(clean/filtering-insanity
+          {:protein_id #(str/starts-with? % "#")})
+         clean/numerify)
+        conj
+        [])))
+
+
+
+(let [data (map #(select-keys
+                  %
+                  [:protein_id
+                   :norm_rel_fc_protein_2_5_transformed
+                   :norm_rel_fc_protein_10_transformed
+                   :norm_rel_fc_protein_50_transformed
+                   :norm_rel_fc_protein_250_transformed
+                   :temperature
+                   :pec50
+                   :slope
+                   :r_sq])
+                data)
+      data-to-write (concat [(map name (keys (first data)))]
+                            (map vals data))]
+  (with-open [writer (io/writer "out-file.csv")]
+    (csv/write-csv writer
+                   data-to-write))
+  data-to-write)
+
+(field-domain :protein_id data)
 
 (oz/start-server!)
 
-(defn play-data
-  [& names]
-  (for [n names
-        i (range 20)]
-    {:time i :item n :quantity (+ (Math/pow (* i (count n)) 0.8) (rand-int (count n)))}))
-
 (def line-plot
-  {:data     {:values (play-data "monkey" "slipper" "broom")}
-   :encoding {:x     {:field "time" :type "quantitative"}
-              :y     {:field "quantity" :type "quantitative"}
-              :color {:field "item" :type "nominal"}}
-   :mark     "line"})
+  {:data      {:values (->> processed
+                            (map (fn [m]
+                                   (assoc m :url (str "https://www.uniprot.org/uniprotkb?query=" (:clustername m)))))
+                            #_(map (fn [m]
+                                     (select-keys m [:slopeH1 :rssH0 :rssH1 :F_statistic]))))}
+   :params    [{:name   "effect_type",
+                :select {:type "point", :fields ["detected_effectH1"]},
+                :bind   "legend"}],
+   :transform [{:calculate "(datum.slopeH1 > 0 ? 1 : -1) * sqrt(datum.rssH0 - datum.rssH1)",
+                :as        "effect_size"},
+               {:calculate "log(datum.F_statistic + 1) / log(2)",
+                :as        "log_transformed_F_statistic"}]
+   :encoding  {:x           {:field "effect_size",
+                             :type  "quantitative",
+                             :title "Effect Size"},
+               :y           {:field "log_transformed_F_statistic",
+                             :type  "quantitative",
+                             :title "Log2(F-statistic + 1)"}
+               :color       {:field "detected_effectH1"
+                             :type  "nominal"
+                             :title "Effect type"}
+               :opacity     {:condition {:param "effect_type", :value 1},
+                             :value     0}
+               :tooltip [{:field "clustername", :title "Gene Cluster Name" :type "nominal"}
+                         {:field "effect_size", :title "Fold Change" :type "quantitative"}]
+               :href        {:field "url", :type "nominal"}}
+   :mark      #_{:type "point", :tooltip true} "point"
+   :width 400,
+   :height 400,})
 
-;; Render the plot
 (oz/view! line-plot)
-
-
-(def stacked-bar
-  {:data     {:values (play-data "munchkin" "witch" "dog" "lion" "tiger" "bear")}
-   :mark     "bar"
-   :encoding {:x     {:field "time"
-                      :type  "ordinal"}
-              :y     {:aggregate "sum"
-                      :field     "quantity"
-                      :type      "quantitative"}
-              :color {:field "item"
-                      :type  "nominal"}}})
-
-(oz/view! stacked-bar)
-
-
-(def contour-plot (oz/load "examples/contour-lines.vega.json"))
-(oz/view! contour-plot :mode :vega)
 
 (def viz
   [:div
-    [:h1 "Look ye and behold"]
-    [:p "A couple of small charts"]
-    [:div {:style {:display "flex" :flex-direction "row"}}
-      [:vega-lite line-plot]
-      [:vega-lite stacked-bar]]
-    [:p "A wider, more expansive chart"]
-    [:vega contour-plot]
-    [:h2 "If ever, oh ever a viz there was, the vizard of oz is one because, because, because..."]
-    [:p "Because of the wonderful things it does"]])
+   [:h3 "TPP Cefotaxime E.Coli"]
+   [:div {:style {:display        "flex"
+                  :flex-direction "row"}}
+    [:vega-lite line-plot]]])
 
 (oz/view! viz)
 
