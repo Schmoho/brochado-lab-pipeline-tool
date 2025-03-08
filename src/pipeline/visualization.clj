f263d4eae00a035e432ac482f3b36f0b152c44f3
Dario <d.eltzner@gmx.de>
2025-03-08 16:59:06 +0100

    return of frankenstein

--- src/pipeline/visualization.clj
+++ src/pipeline/visualization.clj
@@ -21,7 +21,6 @@
   (api.uniprot/uniprotkb-stream {:query "taxonomy_id:83333"}))
 
 
-
 (defn with-uniprot-id-fn
   [id-lookup]
   (fn [table-row]
@@ -47,20 +46,26 @@
   {:data      {:values data}
    :params    [{:name   "p_effect_type",
                 :select {:type "point", :fields ["effect_type"]},
-                :bind   "legend"}]
+                :bind   "legend"}
+               {:name  "p_fdr_threshold",
+                :value 0.05,
+                :bind  {:input "range", :min 0, :max 1, :step 0.01}}
+               {:name   "grid",
+                :select "interval",
+                :bind   "scales"}]
    :encoding  {:x       {:field "effect_size",
                          :type  "quantitative",
-                         :title "Effect Size"
-                         :scale {:domain [-8, 8],
-                                 :nice false}},
+                         :title "Effect Size"},
                :y       {:field "log_transformed_f_statistic",
                          :type  "quantitative",
                          :title "Log2(F-statistic + 1)"}
                :color   {:field "effect_type",
                          :type  "nominal"
                          :title "Effect type"}
-               :opacity {:condition {:param "p_effect_type", :value 1},
-                         :value     0}
+               :opacity {:condition [{:test "(datum.fdr <= p_fdr_threshold)",
+                                      :value 1}
+                                     #_{:param "p_effect_type", :value 1}]
+                         :value 0.2}
                :tooltip [{:field "gene_name", :title "Gene Name" :type "nominal"}
                          {:field "effect_size", :title "Fold Change" :type "quantitative"}]
                :href    {:field "url", :type "nominal"}}
@@ -68,22 +73,115 @@
    :width     600,
    :height    400})
 
-(def viz
-  (let [data (->> cefotaxime-ecoli
-                  (map (with-uniprot-id-fn
-                         (comp
-                          :primaryAccession
-                          (uniprot/proteome-lookup proteome))))
-                  #_(filter (go-term-filtering-fn proteome "GO:0009252"))
-                  (map (fn [m]
-                         (assoc m :url
-                                (some->> (:uniprot-id m)
-                                         (str "https://www.uniprot.org/uniprotkb/"))))))]
-    [:div
-     [:h3 "TPP Cefotaxime E.Coli"]
-     [:div {:style {:display        "flex"
-                    :flex-direction "row"}}
-      [:vega-lite (volcano-plot data)]]]))
+
+(defn volcano-cross-highlight
+  [data]
+  {:data     {:values data}
+   :hconcat [{:width    700,
+              :height   600,
+              :params   [{:name   "selectedGenes",
+                          :select {:type "interval", :encodings ["x" "y"]}}
+                         {:name   "p_effect_type",
+                          :select {:type "point", :fields ["effect_type"]},
+                          :bind   "legend"}
+                         {:name  "p_fdr_threshold",
+                          :value 0.05,
+                          :bind  {:input "range", :min 0, :max 1, :step 0.01}}
+                         #_{:name   "grid",
+                            :select "interval",
+                            :bind   "scales"}],
+              :mark     "point",
+              :encoding {:x       {:field "effect_size_1",
+                                   :type  "quantitative",
+                                   :title "Effect Size 1"},
+                         :y       {:field "log_transformed_f_statistic_1",
+                                   :type  "quantitative"},
+                         :opacity {:condition {:param "selectedGenes"
+                                               :value 1},
+                                   :value     0.2}
+                         :color   {:field "effect_type_1",
+                                   :type  "nominal"
+                                   :title "Effect type"},
+                         :tooltip [{:field "gene_name", :type "nominal", :title "Gene"}]}}
+             {:width    700,
+              :height   600,
+              :mark     "point",
+              :encoding {:x       {:field "effect_size_2",
+                                   :type  "quantitative",
+                                   :title "Effect Size 2"},
+                         :y       {:field "log_transformed_f_statistic_2",
+                                   :type  "quantitative"},
+                         :opacity {:condition {:param "selectedGenes"
+                                               :value 1},
+                                   :value     0.2}
+                         :color   {:field "effect_type_1",
+                                   :type  "nominal"
+                                   :title "Effect type"},
+                         :tooltip [{:field "gene_name", :type "nominal", :title "Gene"}]}}
+             {:width 400,
+              :height 100,
+              :mark "text",
+              :encoding {:text {:signal "selectedGenes && selectedGenes.length > 0 ? 'Selected Genes: ' + selectedGenes.map(d => d.gene_name).join(', ') : 'No genes selected'"}}}]})
+
+
+(defn volcano-cross-plot
+  [cross-data {:keys [x-label
+                      y-label]}]
+  {:data     {:values cross-data}
+   :params   [{:name  "p_fdr_threshold",
+               :value 0.05,
+               :bind  {:input "range", :min 0, :max 1, :step 0.01}}
+              {:name   "grid",
+               :select "interval",
+               :bind   "scales"}]
+   :vconcat
+   [{:width  400,
+     :height 80,
+     :transform [{:filter "datum.fdr_1 <= p_fdr_threshold && datum.fdr_2 <= p_fdr_threshold"}]
+
+     :mark   {:type "bar" :color "orange"}
+     :encoding {:x {:field "effect_size_1",
+                    :bin   {:maxbins 400},
+                    :type  "quantitative",
+                    :axis  nil},
+                :y {:aggregate "count", :type "quantitative"}}}
+    {:hconcat [{:width    400,
+                :height   400,
+                :params   [{:name   "p_effect_type_1",
+                            :select {:type "point", :fields ["effect_type_1"]},
+                            :bind   "legend"}
+                           {:name   "p_effect_type_2",
+                            :select {:type "point", :fields ["effect_type_2"]},
+                            :bind   "legend"}],
+                :mark     "point",
+                :encoding {:x       {:field "effect_size_1",
+                                     :type  "quantitative",
+                                     :title x-label},
+                           :y       {:field "effect_size_2",
+                                     :type  "quantitative",
+                                     :title y-label},
+                           :color   {:condition
+                                     [{:test  "datum.effect_size_1 >= datum.effect_size_2 + 1",
+                                       :value "steelblue"},
+                                      {:test  "datum.effect_size_2 >= datum.effect_size_1 + 1",
+                                       :value "orange"}],
+                                     :value "gray"},
+                           :opacity {:condition
+                                     {:test
+                                      "datum.fdr_1 > p_fdr_threshold || datum.fdr_2 > p_fdr_threshold",
+                                      :value 0.2},
+                                     :value 1},
+                           :tooltip [{:field "gene_name", :title "Gene Name", :type "nominal"}],
+                           :href    {:field "url", :type "nominal"}}}
+               {:width    80,
+                :height   400,
+                :transform [{:filter "datum.fdr_1 <= p_fdr_threshold && datum.fdr_2 <= p_fdr_threshold"}]
+                :mark     {:type "bar" :color "steelblue"}
+                :encoding {:x {:aggregate "count", :type "quantitative"}
+                           :y {:field "effect_size_2",
+                               :bin   {:maxbins 400},
+                               :type  "quantitative",
+                               :axis  nil}}}]}]})
 
 (def cross-data
   (set/join
@@ -102,125 +200,47 @@
                       :fdr :fdr_2})
    amikacin-ecoli)))
 
-(defn volcano-cross-plot
-  [cross-data {:keys [x-label
-                      y-label]}]
-  {:data     {:values cross-data}
-   :params   [{:name   "p_effect_type_1",
-               :select {:type "point", :fields ["effect_type_1"]},
-               :bind   "legend"}
-              {:name   "p_effect_type_2",
-               :select {:type "point", :fields ["effect_type_2"]},
-               :bind   "legend"}
-              {:name  "p_fdr_threshold",
-               :value 0.05,
-               :bind  {:input "range", :min 0, :max 1, :step 0.01}}
-              {:name   "grid",
-               :select "interval",
-               :bind   "scales"}]
-   :vconcat
-   [{:width  400,
-     :height 80,
-     :mark   "bar",
-     :encoding
-     {:x {:field "effect_size_1",
-          :bin   {:maxbins 20},
-          :type  "quantitative",
-          :axis  nil},
-      :y {:aggregate "count", :type "quantitative"}}}
-    {:hconcat [{:width    400,
-                :height   400,
-                :params   [{:name   "p_effect_type_1",
-                            :select {:type "point", :fields ["effect_type_1"]},
-                            :bind   "legend"}
-                           {:name   "p_effect_type_2",
-                            :select {:type "point", :fields ["effect_type_2"]},
-                            :bind   "legend"}],
-                :mark     "point",
-                :encoding {:x       {:field "effect_size_1",
-                                     :type  "quantitative",
-                                     :title "Cefotaxime Fold Change"},
-                           :y       {:field "effect_size_2",
-                                     :type  "quantitative",
-                                     :title "Amikacin Fold Change"},
-                           :color   {:condition
-                                     [{:test  "datum.effect_size_1 >= datum.effect_size_2 + 1",
-                                     :value "steelblue"},
-                                    {:test  "datum.effect_size_2 >= datum.effect_size_1 + 1",
-                                     :value "orange"}],
-                                     :value "gray"},
-                           :opacity {:condition
-                                     {:test
-                                      "number(datum.fdr_1) < p_fdr_threshold && number(datum.fdr_2) < p_fdr_threshold",
-                                      :value 0.2},
-                                     :value 1},
-                           :tooltip [{:field "gene_name", :title "Gene Name", :type "nominal"}],
-                           :href    {:field "url", :type "nominal"}}}
-               {:width    80,
-                :height   400,
-                :mark     "bar",
-                :encoding {:x {:aggregate "count", :type "quantitative"}
-                           :y {:field "effect_size_2",
-                               :bin   {:maxbins 20},
-                               :type  "quantitative",
-                               :axis  nil}}}]}]
-   :encoding {:x       {:field "effect_size_1",
-                        :type  "quantitative",
-                        :title x-label}
-              :y       {:field "effect_size_2",
-                        :type  "quantitative",
-                        :title y-label},
-              :color   {:condition [{:test  "datum.effect_size_1 >= datum.effect_size_2 + 1",
-                                     :value "steelblue"},
-                                    {:test  "datum.effect_size_2 >= datum.effect_size_1 + 1",
-                                     :value "orange"}]
-                        :value     "gray"},
-              :opacity {:condition
-                        {:test  "datum.fdr_1 > p_fdr_threshold || datum.fdr_2 > p_fdr_threshold",
-                         :value 0.2},
-                        :value 1},
-              :tooltip [{:field "gene_name", :title "Gene Name" :type "nominal"}
-                        {:field "fdr_1", :title "FDR Cefotaxime" :type "nominal"}
-                        {:field "fdr_2", :title "FDR Amikacin" :type "nominal"}]
-              :href    {:field "url", :type "nominal"}}
-   :mark     "point"
-   :width    700,
-   :height   500})
-
-{:params
- [{:name  "p_fdr_threshold",
-   :value 0.05,
-   :bind  {:input "range", :min 0, :max 1, :step 0.01}}],
- }
+(defn plotify
+  [data]
+  (->> data
+       (map (with-uniprot-id-fn
+              (comp
+               :primaryAccession
+               (uniprot/proteome-lookup proteome))))
+       #_(filter (go-term-filtering-fn proteome "GO:0009252"))
+       (map (fn [m]
+              (assoc m :url
+                     (some->> (:uniprot-id m)
+                              (str "https://www.uniprot.org/uniprotkb/")))))))
 
 (def cross-viz
-  (let [cross-data (->> cross-data
-                        (filter #(and (pos? (:fdr_1 %))
-                                      (pos? (:fdr_2 %))))
-                        (map (with-uniprot-id-fn
-                               (comp
-                                :primaryAccession
-                                (uniprot/proteome-lookup proteome))))
-                        #_(filter (go-term-filtering-fn proteome "GO:0009252"))
-                        (map (fn [m]
-                               (assoc m :url
-                                      (some->> (:uniprot-id m)
-                                               (str "https://www.uniprot.org/uniprotkb/"))))))]
+  (let [cefotaxime (plotify cefotaxime-ecoli)
+        amikacin   (plotify amikacin-ecoli)
+        cross-data (plotify cross-data)]
     [:div
      [:h3 "Cefotaxime vs. Amikacin E.Coli"]
+     #_[:div {:style {:display        "flex"
+                      :flex-direction "row"}}
+        [:vega-lite (volcano-plot cefotaxime)]
+        [:vega-lite (volcano-plot amikacin)]]
      [:div {:style {:display        "flex"
                     :flex-direction "row"}}
-      [:vega-lite (volcano-cross-plot cross-data
-                                      {:x-label "Cefotaxime Fold Change"
-                                       :y-label "Amikacin Fold Change"})]]]))
+      [:vega-lite (volcano-cross-highlight cross-data)]]
+     #_[:div {:style {:display        "flex"
+                      :flex-direction "row"}}
+        [:vega-lite (volcano-cross-plot cross-data
+                                        {:x-label "Cefotaxime Fold Change"
+                                         :y-label "Amikacin Fold Change"})]]]))
+
+
 
 
 (comment
   (oz/start-server!)
-  
-  (oz/view! viz)
 
   (oz/view! cross-viz)
+
+  (oz/view! viz)
 
   (->> (map uniprot/go-terms-in-protein proteome)
        (apply concat)
@@ -238,5 +258,37 @@
      :temperature
      :pec50
      :slope
-     :r_sq]))
+     :r_sq])
+
+  {:width 80,
+   :height 400,
+   :transform [{:density "effect_size_2",
+                :as ["value", "density"]}]
+
+   :mark "line",
+   :encoding {:y {:field "value",
+                  :type "quantitative",
+                  :axis nil},
+              :x {:field "density",
+                  :type "quantitative"
+                  :sort "-y"}}}
+
+  
+(def viz
+  (let [data (->> cefotaxime-ecoli
+                  (map (with-uniprot-id-fn
+                         (comp
+                          :primaryAccession
+                          (uniprot/proteome-lookup proteome))))
+                  #_(filter (go-term-filtering-fn proteome "GO:0009252"))
+                  (map (fn [m]
+                         (assoc m :url
+                                (some->> (:uniprot-id m)
+                                         (str "https://www.uniprot.org/uniprotkb/"))))))]
+    [:div
+     [:h3 "TPP Cefotaxime E.Coli"]
+     [:div {:style {:display        "flex"
+                    :flex-direction "row"}}
+      [:vega-lite (volcano-plot data)]]])))
+
 
